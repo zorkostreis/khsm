f619a4223ec7ea30ffc03b6fd919c15da77d5791
Eva Israelian <evais1@mail.ru>
2022-09-13 20:25:21 +0300

    current_game_question/previous_level test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -107,4 +107,16 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:money)
     end
   end
+
+  describe '#current_game_question' do
+    it 'returns correct current game question' do
+      expect(game_w_questions.current_game_question).to eq game_w_questions.game_questions.first
+    end
+  end
+
+  describe '#previous_level' do
+    it 'returns correct previous level' do
+      expect(game_w_questions.previous_level).to eq(game_w_questions.current_level - 1)
+    end
+  end
 end
